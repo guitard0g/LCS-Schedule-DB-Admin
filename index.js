c4f5f70535229ec67cd4d82d3c903f11d68ad2fe
Josh Learn <joshuaryanlearn@gmail.com>
2017-07-16 22:51:41 -0400

    tryna get bootstrap to work

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use('/api', api); // redirect API calls
+app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
