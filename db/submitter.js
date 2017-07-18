b35d987572a0fb3777100ed7e03e4fd3fd1fb0c4
Josh Learn <joshuaryanlearn@gmail.com>
2017-07-17 23:48:38 -0400

    stuff

--- db/submitter.js
+++ db/submitter.js
@@ -24,9 +24,18 @@ var testAdd = (db, body) => {
 	if(body.day.length < 2)
 		date = date + '0';
 	date = date + body.day;
+	var oldData = "";
+  ref.orderByKey().equalTo(date).on("child_added", function(snapshot) {
+    if (snapshot === undefined) {
+			oldData = "";
+    }
+    else {
+			oldData = snapshot.val().matches;
+    }
+  });
 	ref.update({
 		[date]: {
-  		matches: body.team1 +'-'+ body.team2 +'-'+ body.time +'-'+ body.victor
+  		matches: oldData + ";" + body.team1 +'-'+ body.team2 +'-'+ body.time +'-'+ body.victor
   	}
 	});
 };
